556523f38ae0f5a2cc0f05ba3a9bed551c139a4c
Gustavo Franco <gufranco@users.noreply.github.com>
2020-03-16 14:26:57 -0300

    Fix TypeORM fixtures

--- ormconfig.js
+++ ormconfig.js
@@ -1,4 +1,4 @@
-// Only used by TypeORM CLI commands
+// TypeORM CLI
 
 module.exports = {
   type: 'postgres',
@@ -14,4 +14,8 @@ module.exports = {
     process.env.TYPEORM_DATABASE && process.env.NODE_ENV
       ? `${process.env.TYPEORM_DATABASE}_${process.env.NODE_ENV}`
       : 'mandabrejas_development',
+  logging: process.env.NODE_ENV !== 'production',
+  entities: [`${__dirname}/dist/entities/*.js`],
+  migrations: [`${__dirname}/dist/migrations/*.js`],
+  subscribers: [`${__dirname}/dist/subscribers/*.js`],
 };
