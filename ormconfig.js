7b6732898580fa40cc1d24dc001a76cd511fa404
Gustavo Franco <gufranco@users.noreply.github.com>
2020-03-30 13:17:11 -0300

    Altera o TypeORM CLI para sempre exibir logs

--- ormconfig.js
+++ ormconfig.js
@@ -14,7 +14,7 @@ module.exports = {
     process.env.TYPEORM_DATABASE && process.env.NODE_ENV
       ? `${process.env.TYPEORM_DATABASE}_${process.env.NODE_ENV}`
       : 'mandabrejas_development',
-  logging: process.env.NODE_ENV !== 'production',
+  logging: true,
   entities: [`${__dirname}/dist/entities/*.js`],
   migrations: [`${__dirname}/dist/migrations/*.js`],
   subscribers: [`${__dirname}/dist/subscribers/*.js`],
